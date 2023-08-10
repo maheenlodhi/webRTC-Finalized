71ffb4d70325ea91cf0729fa1307df5fc9f94e84
maheenlodhi <maheenkhan4837@gmail.com>
2023-08-10 18:04:40 +0500

    try

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
-const socket = io("/");
+const socket = io("http://localhost:4000");
 const videoGrid = document.getElementById("video-grid");
+const shareScreen = document.getElementById("share-screen");
 const myPeer = new Peer(undefined, {
   host: "/",
   port: "3001",
@@ -11,13 +12,14 @@ myVideo.muted = true;
 const connectedPeers = {};
 
 myPeer.on("open", (id) => {
+  user_Id = id;
   socket.emit("join-room", ROOM_ID, id);
 });
 
 const connectNewUser = (userId, stream) => {
   const call = myPeer.call(userId, stream);
-
   const video = document.createElement("video");
+
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
@@ -34,28 +36,49 @@ socket.on("user-disconnected", (userId) => {
   if (connectedPeers[userId]) connectedPeers[userId].close();
 });
 
-navigator.mediaDevices
-  .getUserMedia({
-    // video: true,
-    audio: true,
-  })
-  .then((stream) => {
-    addVideoStream(myVideo, stream);
+shareScreen.addEventListener("click", async () => {
+  try {
+    const screenStream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      audio: true,
+    });
 
+    addVideoStream(screenStream, myVideo);
     myPeer.on("call", (call) => {
       call.answer(stream);
       const video = document.createElement("video");
       call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream);
-        connsole.log("user video stream");
+        console.log("user video stream");
       });
     });
+  } catch (error) {
+    console.error("Error sharing screen:", error);
+  }
+});
 
-    socket.on("user-connected", (userId) => {
-      console.log("userConnected", userId);
-      connectNewUser(userId, stream);
-    });
-  });
+// navigator.mediaDevices
+//   .getUserMedia({
+//     video: false,
+//     audio: true,
+//   })
+//   .then((stream) => {
+//     addVideoStream(myVideo, stream);
+
+//     myPeer.on("call", (call) => {
+//       call.answer(stream);
+//       const video = document.createElement("video");
+//       call.on("stream", (userVideoStream) => {
+//         addVideoStream(video, userVideoStream);
+//         console.log("user video stream");
+//       });
+//     });
+
+//     socket.on("user-connected", (userId) => {
+//       console.log("userConnected", userId);
+//       connectNewUser(userId, stream);
+//     });
+//   });
 
 function addVideoStream(video, stream) {
   video.srcObject = stream;
@@ -63,4 +86,31 @@ function addVideoStream(video, stream) {
     video.play();
   });
   videoGrid.append(video);
+
+  // if (stream.getVideoTracks().length > 0) {
+  //   videoGrid.append(video);
+  // } else {
+  //   const screenVideoContainer = document.getElementById("screen-video");
+  //   screenVideoContainer.srcObject = stream;
+  //   screenVideoContainer.addEventListener("loadedmetadata", () => {
+  //     screenVideoContainer.play();
+
+  //     screenVideoContainer.appendChild(video);
+  //   });
+  // }
 }
+
+// function shareScreenStream(screenStream, user_Id) {
+//   const call = myPeer.call(user_Id, screenStream);
+
+//   const screenVideo = document.createElement("video");
+//   call.on("stream", (userScreenStream) => {
+//     addVideoStream(screenVideo, userScreenStream);
+//   });
+
+//   call.on("close", () => {
+//     screenVideo.remove();
+//   });
+
+//   connectedPeers[user_Id] = call;
+// }
